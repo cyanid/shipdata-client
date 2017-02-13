daeac529382ff720cf1e461e8568f54148d89877
Teemu Huttunen <teemu@huttunen.fi>
2017-02-13 21:46:03 +0200

    general fixes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,51 +8,58 @@
  * Controller of the shipdataClientApp
  */
 angular.module('shipdataClientApp')
-  .controller('MainCtrl', function ($scope, Restangular, amMoment) {
-    amMoment.changeLocale('fi');
+    .controller('MainCtrl', function ($scope, Restangular, amMoment) {
+        amMoment.changeLocale('fi');
 
-    $scope.shipDataReady = false;
-    $scope.shipData = {};
-    $scope.moreShipData = [];
-    $scope.nextShipData = {};
-    $scope.previousShipData = {};
-    $scope.moreShips = false;
+        $scope.shipDataReady = false;
+        $scope.shipData = {};
+        $scope.moreShipData = [];
+        $scope.nextShipData = {};
+        $scope.previousShipData = {};
+        $scope.moreShips = false;
 
-    $scope.shipAmountOffset = 0;
+        $scope.shipAmountOffset = 0;
 
-    $scope.getMoreShips = function () {
-      //console.log('get more ships');
-      $scope.shipAmountOffset = $scope.shipAmountOffset + 3;
+        $scope.getMoreShips = function () {
+            //console.log('get more ships');
+            $scope.shipAmountOffset = $scope.shipAmountOffset + 3;
 
-      $scope.moreShipDataShip = $scope.shipData[1].shipName;
-      $scope.moreShipDataFirm = $scope.shipData[1].firmName;
-      $scope.moreShipDataArrivalTime = $scope.shipData[1].arrivalTime;
+            $scope.moreShipDataShip = $scope.shipData[1].shipName;
+            $scope.moreShipDataFirm = $scope.shipData[1].firmName;
 
-      $scope.moreShips = true;
+            var arrivalTime = $scope.shipData[1].arrivalTime;
+            arrivalTime = arrivalTime.substr(3, 2) + '.' + arrivalTime.substr(0, 2) + '. ' + arrivalTime.substr(6, 5);
 
-      //console.log($scope.moreShipData);
-    };
+            $scope.moreShipDataArrivalTime = arrivalTime;
 
-    $scope.fetchCurrent = function () {
-      //console.log('fetch current');
-      $scope.shipDataReady = false;
+            $scope.moreShips = true;
+        };
 
-      $scope.shipAmountOffset = 0;
+        $scope.fetchCurrent = function () {
+            //console.log('fetch current');
+            $scope.shipDataReady = false;
 
-      var currentShips = Restangular.all('shipData');
-      currentShips.getList().then(function (shipData) {
-        //console.log('got ship data');
-        //console.log(shipData[0]);
+            $scope.shipAmountOffset = 0;
 
-        $scope.nextShipData.shipName = shipData[0].shipName;
-        $scope.nextShipData.firmName = shipData[0].firmName;
-        $scope.nextShipData.arrivalTime = shipData[0].arrivalTime;
+            var currentShips = Restangular.all('shipData');
+            currentShips.getList().then(function (shipData) {
+                //console.log('got ship data');
+                //console.log(shipData[0]);
 
-        $scope.shipData = shipData;
-        $scope.shipDataReady = true;
+                $scope.nextShipData.shipName = shipData[0].shipName;
+                $scope.nextShipData.firmName = shipData[0].firmName;
 
-      });
-    };
+                var arrivalTime = shipData[0].arrivalTime;
 
-    $scope.fetchCurrent();
-  });
+                arrivalTime = arrivalTime.substr(3, 2) + '.' + arrivalTime.substr(0, 2) + '. ' + arrivalTime.substr(6, 5);
+
+                $scope.nextShipData.arrivalTime = arrivalTime;
+
+                $scope.shipData = shipData;
+                $scope.shipDataReady = true;
+
+            });
+        };
+
+        $scope.fetchCurrent();
+    });
